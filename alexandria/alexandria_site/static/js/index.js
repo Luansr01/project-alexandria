f42a2abbdfde73bc835ccc47d778e5e7a0c2e56a
Luan <luan.sr2010@gmail.com>
2023-03-22 22:45:11 -0300

    Added a shading effect when opening sidenav and made some changes to the About page

--- alexandria/alexandria_site/static/js/index.js
+++ alexandria/alexandria_site/static/js/index.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
+    document.getElementById("shade").style.backgroundColor = "#00000050"
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("shade").style.backgroundColor = "#00000000"
 }
 
 function makeBig(size){
